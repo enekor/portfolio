4acd098b8be1f72a8a237fa43f8eb4626d8335ab
enekorebollo <eneko.rebollo@airtificial.com>
2024-06-12 08:57:00 +0200

    titulos y tecnologias

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBriefcase, faEnvelope, faGear, faPen, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { Experience } from '../../models/experience';
 import { CommonModule } from '@angular/common';
+import { Titles } from '../../models/Titles';
 
 @Component({
   selector: 'app-home',
@@ -21,5 +22,40 @@ export class HomeComponent {
   experiencias = [
     new Experience("Originpath","2022",[".NET","Python","Angular","Java","SpringBoot"],"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS3_OMiz8nP0c14NvTZ-rPSYfgIDtOVW6sNQ&s"),
     new Experience("Inypsa Airtificial","2022-Actualidad",[".NET","Python","Angular","Blazor"],"https://pcbolsa.com/Logos/M_ES0152768612.jpg")
+  ]
+
+  titulos = [
+    new Titles("Desarrollo de aplicaciones multiplataforma",""),
+    new Titles("EFSET C2","https://github.com/enekor/MisCertificados/blob/almacenamiento/certificados/EF%20SET%20Certificate.pdf"),
+    new Titles("Certificados android","https://github.com/enekor/MisCertificados/almacenamiento/certificados/Android"),
+    new Titles("Certificados API","https://github.com/enekor/MisCertificados/almacenamiento/certificados/Api"),
+    new Titles("Certificados Java/Kotlin","https://github.com/enekor/MisCertificados/almacenamiento/certificados/JavaKotlin"),
+    new Titles("Certificados Git","https://github.com/enekor/MisCertificados/almacenamiento/certificados/Git"),
+    new Titles("Certificados Javascript","https://github.com/enekor/MisCertificados/almacenamiento/certificados/JS"),
+  ]
+
+  lenguajes = [
+    "c%23-%23239120",
+    "dart-%230175C2",
+    "html5-%23E34F26",
+    "kotlin-%237F52FF",
+    "markdown-%23000000",
+    "python-3670A0",
+    "firebase-%23039BE5",
+    ".NET-5C2D91",
+    "blazor-%235C2D91",
+    "bootstrap-%238511FA",
+    "Flutter-%2302569B",
+    "mysql-%2300000f",
+    "Firebase-039BE5",
+    "MariaDB-003545",
+    "sqlite-%2307405e",
+    "postgres-%23316192",
+    "docker-%230db7ed",
+    "sonarqube-4E9BCD",
+    "Gradle-02303A",
+    "Postman-FF6C37",
+    "SonarQube-black",
+    "java-%23ED8B00"
   ]
 }
---
+++ src/models/Titles.ts
@@ -0,0 +1,9 @@
+export class Titles{
+    Nombre:string = ""
+    Url:string = ""
+
+    constructor(nombre:string,url:string){
+        this.Nombre = nombre
+        this.Url = url
+    }
+}
