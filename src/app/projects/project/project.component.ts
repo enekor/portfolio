38871efcd803972806967e0480df1f3a584098e1
eneko12rebollo <eneko12rebollo@gmail.com>
2024-06-18 15:08:54 +0000

    falta el bindeo de datos de ts a html

--- src/app/projects/project/project.component.ts
+++ src/app/projects/project/project.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, input } from '@angular/core';
+import { Project } from '../../../models/Project';
 
 @Component({
   selector: 'app-project',
@@ -9,11 +10,10 @@ import { Component, Input, input } from '@angular/core';
   styleUrl: './project.component.css'
 })
 export class ProjectComponent {
-  @Input() titulo:string = ""
-  @Input() descripcion:string = ""
-  @Input() imagen:string = ""
-  @Input() tecnologias:string[] = []
+  @Input() project:Project = new Project("","", "",[],"")
   @Input() id:number = 0
+  @Input() onMouseEnter:Function = (p:Project) => {}
+  @Input() onMouseLeave:Function = (p:Project) => {}
 
   onHover() {
     let body = document.getElementById(this.id.toString())
@@ -23,6 +23,8 @@ export class ProjectComponent {
     body = document.getElementById('c'+this.id.toString())
     body?.classList.add("open")
     body?.classList.remove('hidden')
+
+    this.onMouseEnter(this.project)
 
   }
 
@@ -35,7 +37,7 @@ export class ProjectComponent {
     body?.classList.remove("open")
     body?.classList.add('hidden')
 
-
+    this.onMouseLeave(this.project)
   }
 
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,6 +3,7 @@ import { Project } from '../../models/Project';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
 import { ProjectComponent } from "./project/project.component";
+import { Console, log } from 'console';
 
 @Component({
     selector: 'app-projects',
@@ -12,10 +13,22 @@ import { ProjectComponent } from "./project/project.component";
     imports: [FontAwesomeModule, CommonModule, ProjectComponent]
 })
 export class ProjectsComponent {
-  selectedProject?:Project = undefined
+  selectedProject:Project = new Project("Seleccione un proyecto para ver mas información","", "",[],"")
   projects = [
     new Project("Gastoscopio","/projects/gastoscopio.jpg","https://github.com/enekor/Gastoscopio",["Flutter","Firebase"],"Aplicación de gestión de gastos"),
     new Project("BTools","/projects/btools.jpg","https://github.com/enekor/BTools",["Flutter","Firebase"],"Aplicación de herramientas básicas"),
     new Project("PokeCounter","/projects/pokecounter.jpg", "https://github.com/enekor/PokeCounter",["Kotlin","Android Jetpack"],"Aplicación de conteo de racha de pokemon")
   ]
+
+  onMouseEnter(p:Project){
+    console.log('Pito gigante')
+
+    this.selectedProject = p
+    console.log(this.selectedProject)
+    
+  }
+
+  onMouseLeave(p:Project){
+    this.selectedProject = new Project("Seleccione un proyecto para ver mas información","", "",[],"")
+  }
 }
