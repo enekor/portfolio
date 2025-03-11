c431c8a72c83377b05f2ea6e8ef814f515ad8467
eneko12rebollo <eneko12rebollo@gmail.com>
2025-03-11 11:00:23 +0000

    avanzado, hay que ver los tamaños del svg a ver por que no se cambian

--- src/app/widgets/body-card/about-me/about-me.component.ts
+++ src/app/widgets/body-card/about-me/about-me.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SkillCardComponent } from "../../skill-card/skill-card.component";
 import { MobileComponent } from "../../icons/mobile/mobile.component";
+import { BackendComponent } from "../../icons/backend/backend.component";
+import { WebComponent } from "../../icons/web/web.component";
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [SkillCardComponent, MobileComponent],
+  imports: [SkillCardComponent, MobileComponent, BackendComponent, WebComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
